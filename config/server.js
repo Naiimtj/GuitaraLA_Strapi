f766ac16cdb208ff50cad8485de3e415b59a9689
Naiimtj <95881391+Naiimtj@users.noreply.github.com>
2022-03-16 11:22:18 +0100

    deployment

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '3e45099fed2985fac4c52647d726d86c'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '84d0b3d2-b193-48de-a7ea-9f291b9b68d5'
+};
